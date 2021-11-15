eb36f3fc65d4b28e2cd74f3fd3a6340b54ec408b
IkkiU2 <troiczkaya02@bk.ru>
2021-11-15 19:21:25 +0300

    1
    1

--- WpfApp6/Entities/User.cs
+++ WpfApp6/Entities/User.cs
@@ -12,7 +12,7 @@ namespace ElectronicDiary.Entities
 
         public int RoleId { get; set; }
         public virtual Role Role { get; set; }
-
+        //
         //public virtual List<Student> Student { get; set; }
         //public virtual List<Teacher> Teachers { get; set; }
     }
