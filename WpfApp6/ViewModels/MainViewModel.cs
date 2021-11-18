e92bfc52a293aa1b862f896da5f636f965c7504a
Хасп Элд <EldHasp@Gmail.com>
2021-11-18 12:49:23 +0300

    Фикс ошибки в Дизайнере - попытка инициализации БД: Добавлена проверка режима использования ViewModel.

--- WpfApp6/ViewModels/MainViewModel.cs
+++ WpfApp6/ViewModels/MainViewModel.cs
@@ -8,10 +8,16 @@ namespace ElectronicDiary.ViewModels
         private ViewMode _viewMode;
         private RelayCommand _viewModeCommand;
 
-        public ViewMode ViewMode { get => _viewMode; set =>Set(ref _viewMode, value); }
+        public ViewMode ViewMode { get => _viewMode; set => Set(ref _viewMode, value); }
 
         public RelayCommand ViewModeCommand => _viewModeCommand ??= new RelayCommand<ViewMode>(mode => ViewMode = mode);
 
-        private readonly DbStudents dbStudents = new DbStudents();
+        private readonly DbStudents dbStudents;
+
+        public MainViewModel()
+        {
+            if (!IsInDesignMode)
+                dbStudents = new DbStudents();
+        }
     }
 }
